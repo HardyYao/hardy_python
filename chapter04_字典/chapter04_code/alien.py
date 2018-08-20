8a51074ff38adeb06b84238d131285b6e454ab50
HardyYao <hardyyao1995@qq.com>
2018-08-20 23:08:29 +0800

    add chapter04

---
+++ chapter04_字典/chapter04_code/alien.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/20 23:05
+# @Author  : HardyYao
+# @FileName: alien.py
+# @Software: PyCharm
+
+dict = {'key1': 'value1', 'key2': 'value2'}
+
+print(dict['key1'])
